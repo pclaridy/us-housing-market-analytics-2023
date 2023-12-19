87e57adde3481aeac3b382e7fe7b0102ebaa3c79
Pamela Claridy <pclaridy@gmail.com>
2023-12-18 21:20:20 -0500

    updates to data_prep.py and data_modeling.py

--- src/data_preprocessing/data_prep.py
+++ src/data_preprocessing/data_prep.py
@@ -3,103 +3,105 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.impute import SimpleImputer
+from sklearn.impute import KNNImputer
 
+# Load data
 df = pd.read_csv("../../data/raw/raw.csv")
 print(df.info())
+
+# Data Cleaning
+df = df[df["Price"].notna() & (df["Price"] != 0)]
+df["State"].fillna("Unknown", inplace=True)
+df["City"].fillna("Unknown", inplace=True)
+df["Street"].fillna("Unknown", inplace=True)
 
 # Transforming ZIP codes
 df["Zipcode"] = df["Zipcode"].astype(str)
 df["Zipcode"] = df["Zipcode"].str.split(".").str[0]
 
-# Calculate the percentage of zeros in each column
+# EDA - Zero and Null Value Analysis
 zero_percentage = (df == 0).sum() / len(df) * 100
+null_percentage = df.isnull().sum() / len(df) * 100
 print("Percentage of Zeros in Each Column:")
 print(zero_percentage)
-
-# Calculate the percentage of null values in each column
-null_percentage = df.isnull().sum() / len(df) * 100
 print("\nPercentage of Null Values in Each Column:")
 print(null_percentage)
 
-# The decision to drop rows with missing or zero 'Price' values is based on the data analysis which revealed that
-# 1.59% of the 'Price' values are missing and 0.20% of the 'Price' values are zeros.
-# Given the relatively low percentages, removing these rows is unlikely to significantly impact the overall dataset
-# while ensuring the integrity and relevance of the data used for further analysis or modeling.
-df = df[df["Price"].notna() & (df["Price"] != 0)]
-
-# Fill missing values for categorical columns
-df["State"].fillna("Unknown", inplace=True)
-df["City"].fillna("Unknown", inplace=True)
-df["Street"].fillna("Unknown", inplace=True)
-
-# Calculate correlations before imputation
+# Correlation Analysis
 correlation_matrix = df[["MarketEstimate", "RentEstimate", "Price"]].corr()
-
-# Display the correlation matrix
 print("Correlation Matrix Before Imputation:")
 print(correlation_matrix)
 
-# Market Estimate and Rent Estimate with Price Correlation
-correlation_est = df["MarketEstimate"].corr(df["Price"])
-correlation_rent = df["RentEstimate"].corr(df["MarketEstimate"])
-print(f"\nCorrelation between MarketEstimate and Price: {correlation_est:.2f}")
-print(f"Correlation between RentEstimate and MarketEstimate: {correlation_rent:.2f}")
-
-# Frequency Encoding for 'City' and 'Street'
+# Frequency Encoding
 df["City_Freq"] = df["City"].map(df["City"].value_counts().to_dict())
 df["Street_Freq"] = df["Street"].map(df["Street"].value_counts().to_dict())
 
-# Set Pandas display options to show numbers as floating-point with 2 decimal places
-pd.set_option("display.float_format", "{:.2f}".format)
-
-print(df.describe())
-
-# Select only numerical columns for the correlation matrix
+# Correlation Matrix Visualization
 numerical_df = df.select_dtypes(include=[np.number])
-
-# Calculate the correlation matrix
-correlation_matrix = numerical_df.corr()
-
-# Plot the correlation matrix using seaborn
 plt.figure(figsize=(12, 8))
-sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap="coolwarm")
+sns.heatmap(numerical_df.corr(), annot=True, fmt=".2f", cmap="coolwarm")
 plt.title("Correlation Matrix of Numerical Variables")
 plt.show()
 
-# Create a copy of the dataframe for imputation
+# Handle Missing Values and Outliers
 imp_df = df.copy()
 
-print(imp_df.isna().sum())
-print((imp_df == 0).sum())
-
-# Replace null values in 'Bedroom' and 'Bathroom' with their respective median values
+# Impute 'Bedroom' and 'Bathroom'
 imp_df["Bedroom"].fillna(imp_df["Bedroom"].median(), inplace=True)
 imp_df["Bathroom"].fillna(imp_df["Bathroom"].median(), inplace=True)
+imp_df.loc[imp_df["Bedroom"] == 0, "Bedroom"] = imp_df["Bedroom"].median()
+imp_df.loc[imp_df["Bathroom"] == 0, "Bathroom"] = imp_df["Bathroom"].median()
 
-# Replace zero values in 'Bedroom' and 'Bathroom' with their respective median values
-imp_df.loc[imp_df["Bedroom"] == 0, "Bedroom"] = imp_df[imp_df["Bedroom"] != 0][
-    "Bedroom"
-].median()
-imp_df.loc[imp_df["Bathroom"] == 0, "Bathroom"] = imp_df[imp_df["Bathroom"] != 0][
-    "Bathroom"
-].median()
+# KNN imputer will automatically handle null values, but first, set zeros to NaN
+df["Area"].replace(0, np.nan, inplace=True)
 
-# Zero imputation for 'Area'
-imp_df["Area"].fillna(0, inplace=True)
+# Create imputer object
+imputer = KNNImputer(n_neighbors=5)
 
-# Advanced imputation for 'MarketEstimate' and 'RentEstimate'
-# Using RandomForestRegressor
-# Note: Excluding the target variable from predictors to avoid data leakage
-predictors = [
-    "Price",
+# Select columns for KNN imputation
+columns_for_imputation = ["Area"]
+
+# Apply KNN imputation
+df[columns_for_imputation] = imputer.fit_transform(df[columns_for_imputation])
+
+
+# Function to handle outliers using IQR
+def handle_outliers(df, columns):
+    for col in columns:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        df[col] = np.where(df[col] < lower_bound, lower_bound, df[col])
+        df[col] = np.where(df[col] > upper_bound, upper_bound, df[col])
+    return df
+
+
+# Apply outlier handling to relevant numerical columns
+numerical_columns = [
     "Bedroom",
     "Bathroom",
     "Area",
-]  # Variables that are highly correlated with 'MarketEstimate' and 'RentEstimate'
+    "Price",
+    "MarketEstimate",
+    "RentEstimate",
+]  # Modify as needed
+imp_df = handle_outliers(imp_df, numerical_columns)
+
+# Ensure predictors are free of NaN values
+predictors = ["Price", "Bedroom", "Bathroom", "Area"]
+for col in predictors:
+    if imp_df[col].isna().any():
+        imp_df[col].fillna(
+            imp_df[col].median(), inplace=True
+        )  # Or use another imputation method
+
+# Advanced Imputation with RandomForestRegressor
+market_model = RandomForestRegressor()
+rent_model = RandomForestRegressor()
 
 # Impute 'MarketEstimate'
-market_model = RandomForestRegressor()
 market_imp = imp_df.dropna(subset=predictors + ["MarketEstimate"])
 market_model.fit(market_imp[predictors], market_imp["MarketEstimate"])
 imp_df.loc[imp_df["MarketEstimate"].isna(), "MarketEstimate"] = market_model.predict(
@@ -107,15 +109,39 @@ imp_df.loc[imp_df["MarketEstimate"].isna(), "MarketEstimate"] = market_model.pre
 )
 
 # Impute 'RentEstimate'
-rent_model = RandomForestRegressor()
 rent_imp = imp_df.dropna(subset=predictors + ["RentEstimate"])
 rent_model.fit(rent_imp[predictors], rent_imp["RentEstimate"])
 imp_df.loc[imp_df["RentEstimate"].isna(), "RentEstimate"] = rent_model.predict(
     imp_df[imp_df["RentEstimate"].isna()][predictors]
 )
 
-# Check for remaining missing values
-print("Missing Values After Enhanced Imputation:")
+# Verify Imputation
+print("\nMissing Values After Enhanced Imputation:")
 print(imp_df.isna().sum())
 
-df.to_pickle("../../data/processed/cleaned_dataset.pkl")
+# Define threshold for dropping data
+threshold = 5  # 5%
+
+# Calculate the percentage of missing values in each column
+missing_percentage = imp_df.isna().sum() / len(imp_df) * 100
+
+# Drop columns with missing values less than the threshold
+for column in imp_df.columns:
+    if missing_percentage[column] < threshold:
+        imp_df.dropna(subset=[column], inplace=True)
+
+# Verify the changes
+print("\nData after dropping rows with <5% missing values:")
+print(imp_df.isna().sum() / len(imp_df) * 100)
+
+# Update 'PPSq' column
+imp_df.loc[imp_df["PPSq"].isna() | (imp_df["PPSq"] == 0), "PPSq"] = imp_df.apply(
+    lambda row: row["Price"] / row["Area"] if row["Area"] > 0 else 0, axis=1
+)
+
+# Verify 'PPSq' Update
+print("\nData after updating 'PPSq' column:")
+print(imp_df.isna().sum() / len(imp_df) * 100)
+
+# Save cleaned dataset
+imp_df.to_pickle("../../data/processed/cleaned_dataset.pkl")
---
+++ src/modeling/data_modeling.py
@@ -0,0 +1,241 @@
+import numpy as np
+import pandas as pd
+import time
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import (
+    mean_squared_error,
+    r2_score,
+    mean_absolute_error,
+    explained_variance_score,
+)
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import (
+    RandomForestRegressor,
+    GradientBoostingRegressor,
+    StackingRegressor,
+)
+from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+
+# Load and preprocess data
+df = pd.read_pickle("../../data/processed/cleaned_dataset.pkl")
+
+# Split data into features (X) and target (y)
+X = df.drop("Price", axis=1)  # Ensure 'Price' is not in features
+y = df["Price"]  # Target variable
+
+# Identify categorical and numerical columns in features
+categorical_cols = X.select_dtypes(include=["object", "category"]).columns
+numerical_cols = X.select_dtypes(include=["int64", "float64"]).columns
+
+# Feature Engineering: Adding Polynomial Features
+poly = PolynomialFeatures(degree=2, include_bias=False, interaction_only=True)
+X_poly = poly.fit_transform(X[numerical_cols])
+
+# Convert Polynomial Feature array to DataFrame with appropriate feature names
+X_poly_df = pd.DataFrame(
+    X_poly,
+    columns=poly.get_feature_names_out(input_features=numerical_cols),
+    index=X.index,
+)
+
+# Merge Polynomial Features with original data
+X = pd.concat([X.drop(numerical_cols, axis=1), X_poly_df], axis=1)
+
+# Convert all column names to string to avoid type mismatch
+X.columns = X.columns.astype(str)
+
+# Update categorical columns after merging
+categorical_cols = X.select_dtypes(include=["object", "category"]).columns
+
+# Preprocessing pipelines
+numerical_pipeline = Pipeline([("scaler", StandardScaler())])
+categorical_pipeline = Pipeline([("onehot", OneHotEncoder(handle_unknown="ignore"))])
+preprocessor = ColumnTransformer(
+    [
+        ("num", numerical_pipeline, numerical_cols),
+        ("cat", categorical_pipeline, categorical_cols),
+    ]
+)
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Model initialization
+models = {
+    "LinearRegression": LinearRegression(),
+    "Lasso": Lasso(),
+    "Ridge": Ridge(),
+    "ElasticNet": ElasticNet(),
+    "DecisionTreeRegressor": DecisionTreeRegressor(),
+    "RandomForestRegressor": RandomForestRegressor(),
+    "GradientBoostingRegressor": GradientBoostingRegressor(),
+    "XGBRegressor": XGBRegressor(),
+    "KNeighborsRegressor": KNeighborsRegressor(),
+    "SVR": SVR(),
+    "MLPRegressor": MLPRegressor(),
+}
+
+# Model training and evaluation with k-fold cross-validation
+results = []
+k = 5  # Number of folds
+
+for name, model in tqdm(models.items()):
+    start_time = time.time()
+
+    # Create a pipeline
+    pipeline = Pipeline([("preprocessor", preprocessor), ("model", model)])
+
+    # Perform k-fold cross-validation
+    cv_scores = cross_val_score(pipeline, X, y, cv=k, scoring="neg_mean_squared_error")
+    cv_rmse_scores = np.sqrt(-cv_scores)
+
+    # Store results
+    results.append(
+        {
+            "Model": name,
+            "CV Mean RMSE": cv_rmse_scores.mean(),
+            "CV Std RMSE": cv_rmse_scores.std(),
+            "CV Time": time.time() - start_time,
+        }
+    )
+
+results_df = pd.DataFrame(results)
+
+# Hyperparameter tuning
+param_grids = {
+    "RandomForestRegressor": {
+        "model__n_estimators": [100, 200, 300],
+        "model__max_depth": [None, 10, 20],
+        "model__min_samples_split": [2, 5, 10],
+        "model__min_samples_leaf": [1, 2, 4],
+    },
+    "GradientBoostingRegressor": {
+        "model__n_estimators": [100, 200, 300],
+        "model__learning_rate": [0.01, 0.1, 0.2],
+        "model__max_depth": [3, 5, 7],
+        "model__min_samples_split": [2, 5, 10],
+        "model__min_samples_leaf": [1, 2, 4],
+    },
+    "SVR": {
+        "model__C": [0.1, 1, 10],
+        "model__gamma": ["scale", "auto"],
+        "model__kernel": ["rbf", "linear", "poly"],
+        "model__degree": [2, 3, 4],
+    },
+    "MLPRegressor": {
+        "model__hidden_layer_sizes": [(50,), (100,), (50, 50)],
+        "model__activation": ["tanh", "relu"],
+        "model__solver": ["sgd", "adam"],
+        "model__alpha": [0.0001, 0.001, 0.01],
+        "model__learning_rate": ["constant", "adaptive"],
+    },
+    "Ridge": {
+        "model__alpha": [0.1, 1, 10],
+        "model__solver": [
+            "auto",
+            "svd",
+            "cholesky",
+            "lsqr",
+            "sparse_cg",
+            "sag",
+            "saga",
+        ],
+    },
+    "Lasso": {
+        "model__alpha": [0.1, 1, 10],
+        "model__max_iter": [1000, 5000, 10000],
+        "model__selection": ["cyclic", "random"],
+    },
+    "ElasticNet": {
+        "model__alpha": [0.1, 1, 10],
+        "model__l1_ratio": [0.2, 0.5, 0.8],
+        "model__max_iter": [1000, 5000, 10000],
+    },
+    "DecisionTreeRegressor": {
+        "model__max_depth": [None, 10, 20, 30],
+        "model__min_samples_split": [2, 5, 10],
+        "model__min_samples_leaf": [1, 2, 4],
+        "model__max_features": ["auto", "sqrt", "log2"],
+    },
+    "XGBRegressor": {
+        "model__n_estimators": [100, 200, 300],
+        "model__learning_rate": [0.01, 0.1, 0.2],
+        "model__max_depth": [3, 5, 7],
+        "model__subsample": [0.5, 0.7, 1.0],
+        "model__colsample_bytree": [0.5, 0.7, 1.0],
+    },
+    "KNeighborsRegressor": {
+        "model__n_neighbors": [3, 5, 7, 10],
+        "model__weights": ["uniform", "distance"],
+        "model__metric": ["euclidean", "manhattan", "minkowski"],
+    },
+}
+
+# Hyperparameter tuning
+tuned_results = []
+for name, model_instance in tqdm(models.items()):
+    if name in param_grids:
+        # Create the pipeline with the model instance
+        pipeline = Pipeline([("preprocessor", preprocessor), ("model", model_instance)])
+
+        # Conduct the randomized search
+        randomized_search = RandomizedSearchCV(
+            pipeline,
+            param_grids[name],
+            n_iter=10,
+            scoring="neg_mean_squared_error",
+            cv=5,
+            n_jobs=-1,
+        )
+        randomized_search.fit(X_train, y_train)
+
+        # Get the best model and make predictions
+        best_model = randomized_search.best_estimator_
+        y_test_pred = best_model.predict(X_test)
+        test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
+
+        # Append results
+        tuned_results.append(
+            {
+                "Model": name,
+                "Test RMSE": test_rmse,
+                "Best Parameters": randomized_search.best_params_,
+            }
+        )
+
+tuned_results_df = pd.DataFrame(tuned_results)
+
+# Ensemble Model
+estimators = [
+    ("rf", RandomForestRegressor(n_estimators=100, random_state=42)),
+    ("svr", SVR(C=1, gamma="scale")),
+]
+stack_model = StackingRegressor(estimators=estimators, final_estimator=Ridge())
+stack_model.fit(X_train, y_train)
+
+# Additional Evaluation Metrics
+y_pred = stack_model.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+evs = explained_variance_score(y_test, y_pred)
+
+# Determine the best 'k' for KNeighborsRegressor
+param_grid = {"n_neighbors": range(1, 31)}
+knn = KNeighborsRegressor()
+knn_grid = GridSearchCV(knn, param_grid, cv=5, scoring="neg_mean_squared_error")
+knn_grid.fit(X_train, y_train)
+
+print(f"Mean Absolute Error: {mae}")
+print(f"Explained Variance Score: {evs}")
+print("Best k for KNN:", knn_grid.best_params_["n_neighbors"])
+print("Base Model Results:\n", results_df)
+print("\nTuned Model Results:\n", tuned_results_df)
